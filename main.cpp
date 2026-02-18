4fe3e6bcaf6368edecf33d253565b820781ccc81
kseniya-paliichuk <kseniya.paliychuk@gmail.com>
2026-02-18 09:42:06 +0300

    Update main.cpp
    Опять сложный інтерфейс

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
     int n, m, i;
+    cout << "Введите натуральные числа m и n";
     cout << "n= "; cin >> n;
     cout << "m= "; cin >> m;
     for (int i = 1; i < n; i++) {
